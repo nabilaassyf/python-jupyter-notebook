b3981bdc1f5661c69706a8c43bdc5b0aa1b32744
nabilaassyf <98367741+nabilaassyf@users.noreply.github.com>
2022-09-16 20:50:14 +0700

    Add files via upload

---
+++ Assignment SPSF/PR 02/PR 02.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # PR 02
+
+# Contoh #1
+# ![Soal%20PR02%20SPSF.jpeg](attachment:Soal%20PR02%20SPSF.jpeg)
+
+# Contoh #2
+# 
+# Sebuah partikel bermuatan $q$ bergerak dengan kecepatan
+# 
+# $\vec{v}(t) = v_{x}(t)\hat{i}+v_{y}(t)\hat{j}$
+# 
+# dalam ruang bermedan magnetik konstan,
+# 
+# $\vec{B} = -\vec{k}B_{z}$
+# 
+# Tentukan gerak partikelnya!
+
+# In[ ]:
+
+
+
+
